080ade71b05a2da4f97941f4b4b426f5a52358a9
Prashant <agarwalprashant355@gmail.com>
2016-08-29 10:50:39 +0530

    first commit for crime week

---
+++ www/js/app.js
@@ -0,0 +1,172 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.rashtriya', {
+    url: '/rashtriya',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/rashtriya.html',
+        controller:'RashtriyaCtrl'
+      }
+    }
+  })
+
+  .state('app.cricket', {
+      url: '/cricket',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/cricket.html',
+          controller:'CricketCtrl'
+        }
+      }
+    })
+    .state('app.fashion', {
+      url: '/fashion',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/fashion.html',
+          controller: 'FashionCtrl'
+        }
+      }
+    })
+
+  .state('app.fashionId', {
+    url: '/fashion/:fashionId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/fashiondetail.html',
+        controller: 'FashionDetailCtrl'
+      }
+    }
+  })
+
+  .state('app.badiKhabar',{
+    url: '/badiKhabarein',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/badiKhabarein.html',
+        controller:'BadiKhabareinCtrl'
+      }
+    }
+  })
+  .state('app.tazakhabar',{
+    url: '/tazaKhabarein',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/tazaKhabarein.html',
+        controller:'TazaKhabareinCtrl'
+      }
+    }
+  })
+
+.state('app.rajya',{
+    url: '/rajya',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/rajya.html',
+        controller:'RajyaCtrl'
+      }
+    }
+  })
+
+
+.state('app.manoranjan',{
+    url: '/manoranjan',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/manoranjan.html',
+        controller:'ManoranjanCtrl'
+      }
+    }
+  })
+
+
+.state('app.duniya',{
+    url: '/duniya',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/duniya.html',
+        controller:'DuniyaCtrl'
+      }
+    }
+  })
+.state('app.tech',{
+    url: '/tech',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/tech.html',
+        controller:'TechGyaanCtrl'
+      }
+    }
+  })
+.state('app.kaarobar',{
+    url: '/kaarobar',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/kaarobar.html',
+        controller:'KaarobarCtrl'
+      }
+    }
+  })
+.state('app.auto',{
+    url: '/auto',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/auto.html',
+        controller:'AutoCtrl'
+      }
+    }
+  })
+
+.state('app.khel',{
+    url: '/khel',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/khel.html',
+        controller:'KhelCtrl'
+      }
+    }
+  })
+.state('app.zaraHatke',{
+    url: '/zaraHatke',
+    views:{
+      'menuContent':{
+        templateUrl:'templates/hatke.html',
+        controller:'ZaraHatkeCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/fashion');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,99 @@
+angular.module('starter.controllers', ['firebase'])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('FashionCtrl', function($scope,$firebaseArray) {
+var rootRef = firebase.database().ref();
+    var textRef = rootRef.child('MainNews');
+  $scope.allNews = $firebaseArray(rootRef);
+})
+
+
+.controller('FashionDetailCtrl', function($scope, $stateParams) {
+})
+
+.controller("RashtriyaCtrl",function($scope,$firebaseObject){
+var rootRef = firebase.database().ref();
+    var textRef = rootRef.child('notmainnews');
+  $scope.data = $firebaseObject(textRef);
+})
+
+.controller('CricketCtrl', function($scope, $stateParams) {
+})
+
+.controller('BadiKhabareinCtrl', function($scope,$firebaseArray) {
+var rootRef = firebase.database().ref();
+    // var textRef = rootRef.child('MainNews');
+  $scope.allNews = $firebaseArray(rootRef);
+
+})
+
+.controller('TazaKhabareinCtrl', function($scope,$firebaseArray,$firebaseObject) {
+var rootRef = firebase.database().ref();
+    // var textRef = rootRef.child('MainNews');
+  $scope.allNews = $firebaseArray(rootRef);
+$scope.khabarTitle = $firebaseObject(rootRef.child('khabarTitle'));
+})
+
+.controller('RajyaCtrl', function($scope, $stateParams) {
+})
+
+.controller('KhelCtrl', function($scope, $stateParams) {
+})
+
+.controller('AutoCtrl', function($scope, $stateParams) {
+})
+
+.controller('ZaraHatkeCtrl', function($scope, $stateParams) {
+})
+
+.controller('ManoranjanCtrl', function($scope, $stateParams) {
+})
+
+.controller('DuniyaCtrl', function($scope, $stateParams) {
+})
+
+.controller('TechGyaanCtrl', function($scope, $stateParams) {
+})
+
+.controller('KaarobarCtrl', function($scope, $stateParams) {
+});
