2249124439774daa4d1099ca4d9fccecf85939ac
Prashant <agarwalprashant355@gmail.com>
2016-08-30 23:48:11 +0530

    modal working in badi news

--- www/js/app.js
+++ www/js/app.js
@@ -22,6 +22,39 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
   });
 })
 
+
+
+.controller('MediaCtrl', function($scope, $ionicModal) {
+  $scope.allImages = [{
+    'src' : 'img/pic1.jpg'
+  }, {
+    'src' : 'img/pic2.jpg'
+  }, {
+    'src' : 'img/pic3.jpg'
+  }];
+ 
+  $scope.showImages = function(index) {
+    $scope.activeSlide = index;
+    $scope.showModal('templates/image-popover.html');
+  }
+ 
+  $scope.showModal = function(templateUrl) {
+    $ionicModal.fromTemplateUrl(templateUrl, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  }
+ 
+  // Close the modal
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+    $scope.modal.remove()
+  };
+})
+
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
@@ -33,7 +66,6 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
   })
 
   
-    
 
   .state('app.badiKhabar',{
     url: '/badiKhabarein',
