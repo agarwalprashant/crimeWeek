271edec30677f428c83e46dd20c76104861754ba
Prashant <agarwalprashant355@gmail.com>
2016-08-31 10:35:30 +0530

    working app with menu icons

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,14 +42,11 @@ angular.module('starter.controllers', ['firebase'])
 })
 
 
-// badi khabarein controllers
+// badi khabarein controllers  start 
 .controller('BadiKhabareinCtrl', function($scope,ArticleGetter,$ionicModal,$firebaseArray) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 
-
-var rootref = firebase.database().ref();
-badiNewsRef = rootref.child('BadiKhabar');
-  $scope.allImages = $firebaseArray(badiNewsRef);
+  $scope.allImages = $scope.allBadiNews;
  
   $scope.showImages = function(index) {
     $scope.activeSlide = index;
@@ -66,7 +63,7 @@ badiNewsRef = rootref.child('BadiKhabar');
     });
   }
  
-  // Close the modal
+  
   $scope.closeModal = function() {
     $scope.modal.hide();
     $scope.modal.remove()
@@ -80,11 +77,12 @@ badiNewsRef = rootref.child('BadiKhabar');
 $scope.badiDetail = ArticleGetter.getDetailNews('BadiKhabar',$stateParams.badiKhabareinId);
 
 })
+// badi khabarein controllers end
+
+
 
 
 // taza khabarein controllers
-
-
 .controller('TazaKhabareinCtrl', function($scope,ArticleGetter) {
   $scope.allTazaNews = ArticleGetter.getNews('TazaKhabar');
 })
@@ -94,65 +92,122 @@ $scope.badiDetail = ArticleGetter.getDetailNews('BadiKhabar',$stateParams.badiKh
 $scope.TazaDetail = ArticleGetter.getDetailNews('TazaKhabar',$stateParams.tazaKhabareinId);
 
 })
+ //taza khabarein controllers end
 
 
-//rajya news controllers
 
+
+
+
+
+//rajya news controllers start
 .controller('RajyaCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//rajya news controllers end
 
 
+
+
+
+
+//rashtriya news controllers begin
 .controller('RashtriyaCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//rashtriya news controllers end
 
 
 
 
+
+//cricket news controllers begin
 .controller('CricketCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//cricket news controllers end
 
+
+
+
+
+//manoranjan news controllers begin
 .controller('ManoranjanCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//manoranjan news controllers end
 
 
 
+
+
+
+//duniya news controllers begin
 .controller('DuniyaCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//duniya news controllers end
 
+
+
+
+
+//techgyaan news controllers begin
 .controller('TechGyaanCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//techgyaan news controllers end
 
 
 
 
+
+
+//kaarobar news controllers begin
 .controller('KaarobarCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//kaarobar news controllers end
 
+
+
+
+
+//auto news controllers begin
 .controller('AutoCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//auto news controllers end
 
+
+
+
+//khel news controllers begin
 .controller('KhelCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//khel news controllers end
 
 
+
+
+
+//zarahatke news controllers begin
 .controller('ZaraHatkeCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 })
+//zarahatke news controllers end
 
+
+
+
+
+
+//fashion news controllers begin
 .controller('FashionCtrl', function($scope,ArticleGetter) {
   $scope.allBadiNews = ArticleGetter.getNews('BadiKhabar');
 });
-
-
+//fashion news controllers end
 
 
 
